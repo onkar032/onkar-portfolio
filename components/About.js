409e107062b75211bd7df9972401bf718f84a9f8
Onkar Dubey <salesforce.onkar+HFCL@herofincorp.biz>
2025-11-21 15:54:18 +0530

    Remove background effects and grey sections from About Me - clean white background

--- components/About.js
+++ components/About.js
@@ -4,9 +4,8 @@ import { AboutBackground } from './SectionBackgrounds'
 
 export default function About({ data }) {
   return (
-    <section id="about" className="py-24 px-4 bg-white relative overflow-hidden">
-      <AboutBackground />
-      <div className="max-w-7xl mx-auto relative z-10">
+    <section id="about" className="py-24 px-4 bg-white relative">
+      <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
